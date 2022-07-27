a7d0b491d6771cdb4ad4357ae86ec869efc395b9
Shjung <wjdtngus9@gmail.com>
2022-07-27 23:44:26 +0900

    .

--- Begin/src/Test10/SchedulerTest.java
+++ Begin/src/Test10/SchedulerTest.java
@@ -9,7 +9,7 @@ public class SchedulerTest {
 		System.out.println("R : 한명씩 차례대로 할당");
 		System.out.println("L : 쉬고있거나 대기가 가장적은 상담원에게 할당");
 		System.out.println("P : 우선순위가 높은 고객 먼저 할당");
-		
+		System.out.println("A : 상담원이 요청");
 		int ch = System.in.read(); // 할당방식을 입력받아 ch변수에 대입
 		Scheduler scheduler = null;
 		
@@ -21,6 +21,9 @@ public class SchedulerTest {
 		}
 		else if(ch =='p'||ch == 'P') {
 			scheduler = new PriorityAllocation();
+		}
+		else if(ch =='a'||ch == 'A') {
+			scheduler = new AgentGetCall();
 		}
 		else {
 			System.out.println("지원하지 않는 기능입니다.");
