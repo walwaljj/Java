85f5e6e315f4cae72b057ea50151f1a9c6338312
Shjung <wjdtngus9@gmail.com>
2022-08-31 19:59:24 +0900

    .

--- Begin/src/java200/day10.java
+++ Begin/src/java200/day10.java
@@ -7,7 +7,7 @@ public class day10 {
 			{ '`','~','!','@','#','$','%','^','&','*',
 			'(',')','-','_','+','=','|','[',']','{',
 			'}',';',':',',','.','/'};
-		
+						// 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9
 		char[] numCode = {'q','w','e','r','t','y','u','i','o','p'};
 
 		String src = "abc123";
@@ -15,15 +15,18 @@ public class day10 {
 		for(int i=0; i < src.length();i++) {
 			char ch = src.charAt(i);
 			
-				
-			if(ch>=0&&ch<=9) {
-				for(int j=0; j<numCode.length;j++) {
-				System.out.println(numCode[j]);
-				System.out.println("num"+numCode[ch]);
+			if (ch >='0' && ch<='9') {
+				for(int j = 0; j < numCode.length; j++) {
+					result+=numCode[ch-'0'];
+					break;
+				}
+			}else if(ch>='a'&& ch<'z') {
+				for(int k = 0; k < abcCode.length; k++) {
+					result+= abcCode[ch-'a'];
+					break;
+					}
+				}
 			}
-			}
-			
-		}
 		
 		System.out.println("src:"+src);
 		System.out.println("result:"+result);
