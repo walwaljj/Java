7dee6d5edbdcf479bf91ae66493a0097612ebbb9
Shjung <wjdtngus9@gmail.com>
2022-09-23 00:00:01 +0900

    .

--- JavaStudy/src/jungsuk/jungsuk_200/java200_7_19.java
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_7_19.java
@@ -19,25 +19,32 @@ public class java200_7_19 {
 }
 
 class Buyer {
-int money = 1000;
-Product[] cart = new Product[3]; // 구입한 제품을 저장하기 위한 배열
-int i = 0; // Product cart index 배열 에 사용될
-void buy(Product p) {
-}
-void add(Product p) {
-
-} // add(Product p)
-void summary() {
-}
+	int money = 1000;
+	
+	Product[] cart = new Product[3];
+	
+	int i = 0;
+	
+	void buy(Product p) {
+		
+	}
+	void add(Product p) {
+	
+	} 
+	void summary() {
+	}
 }
 class Product {
-int price; // 제품의 가격
-Product(int price) {
-this.price = price;
+	int price;
+	
+	Product(int price) {
+		
+	this.price = price;
+	}
 }
-}
+
 class Tv extends Product {
-Tv() { super(100); }
+	Tv() { super(100); }
 public String toString() { return "Tv"; }
 }
 class Computer extends Product {
