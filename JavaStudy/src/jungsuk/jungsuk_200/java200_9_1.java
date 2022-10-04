28022244261eb92685cfce4e2ba71b6f631c866d
Shjung <wjdtngus9@gmail.com>
2022-10-04 23:56:47 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_9_1.java
@@ -0,0 +1,41 @@
+package jungsuk.jungsuk_200;
+
+
+
+public class java200_9_1 {
+
+	public static void main(String[] args) {
+		SutdaCard4 c1 = new SutdaCard4(3,true);
+		SutdaCard4 c2 = new SutdaCard4(3,true);
+		System.out.println("c1="+c1);
+		System.out.println("c2="+c2);
+		System.out.println("c1.equals(c2):"+c1.equals(c2));
+	}
+
+}
+class SutdaCard4 {
+	int num;
+	boolean isKwang;
+	
+	SutdaCard4() {
+		this(1, true);
+	}
+	SutdaCard4(int num, boolean isKwang) {
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	public boolean equals(Object obj) {
+	/*
+	(1) 매개변수로 넘겨진 객체의num, isKwang  과
+	멤버변수 을 비교하도록 오버라이딩 하시오 num, isKwang .
+	*/
+		if( this.num == ) {
+			
+		}
+		return (this == obj);
+	}
+	public String toString() {
+		return num + ( isKwang ? "K":"");
+	}
+}
