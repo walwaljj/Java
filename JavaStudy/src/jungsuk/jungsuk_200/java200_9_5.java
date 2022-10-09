9ec771b5ce006920c74e1c9afac72557ccecb0ad
Shjung <wjdtngus9@gmail.com>
2022-10-10 00:31:35 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_9_5.java
@@ -0,0 +1,25 @@
+package jungsuk.jungsuk_200;
+
+public class java200_9_5 {
+	
+	public static int count(String src, String target) {
+		int count = 0; // 찾은 횟수
+		int pos = 0; // 찾기 시작할 위치
+		
+		for ( int i = 0 ; i <src.length() ; i++) {
+			
+			pos = src.indexOf(target, pos);
+			if (pos!=-1) {
+				count++;
+				pos += target.length();
+			}else
+				break;
+		}return count;
+	}
+	public static void main(String[] args) {
+		System.out.println(count("12345AB12AB345AB","AB"));
+		System.out.println(count("12345","AB"));
+
+	}
+
+}
