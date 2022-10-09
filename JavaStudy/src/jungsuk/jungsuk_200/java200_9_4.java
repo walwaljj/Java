f004922c5115170f8557212f9ff23943cfcb8633
Shjung <wjdtngus9@gmail.com>
2022-10-10 00:07:29 +0900

    .

--- JavaStudy/src/jungsuk/jungsuk_200/java200_9_4.java
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_9_4.java
@@ -3,10 +3,17 @@ package jungsuk.jungsuk_200;
 public class java200_9_4 {
 	static void printGraph(int[] dataArr, char ch) {
 		
+		for(int i = 0 ; i < dataArr.length ; i++) {
+			for(int j=0 ; j <dataArr[i] ; j++) {
+				System.out.print(ch);
+			}
+			System.out.println(dataArr[i]);
+		}
 	
 	}
 	public static void main(String[] args) {
 		printGraph(new int[]{3,7,1,4},'*');
+
 	}
 	
 
