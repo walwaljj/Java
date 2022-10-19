7988b640af889ba0807cd39d04124bc3d3147e9c
Shjung <wjdtngus9@gmail.com>
2022-10-19 23:35:24 +0900

    .

--- JavaStudy/src/jungsuk/jungsuk_200/java200_9_14.java
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_9_14.java
@@ -24,15 +24,19 @@ public class java200_9_14 {
 			System.exit(0);
 			}
 			
-			
+			String pattern = ".*"+input+".*";
+			Pattern p = Pattern.compile(pattern);
 //			if(Pattern.compile(input)==phoneNumArr[])
 			for (int i = 0 ; i < phoneNumArr.length ; i++) {
 				String str = phoneNumArr[i];
-				if(str.indexOf(input)!=-1)
-					list.add(str);
+				String tmp = str.replace("-","");
+				Matcher m = p.matcher(tmp);
+				// 입력받은 input(패턴화해서 p 에 넣음) 과 phoneNumArr[i](replace해서 특수문자없앰.)을 비교하기위해 matcher에 담음.
+				if(m.find()) {// 위 코드에서 t or f를 확인
+					list.add(str); // t라면 list에 phoneNumArr[i]를 넣는다.
 				
 			}
-			
+			}
 			
 			if(list.size()>0) {
 				System.out.println(list);
