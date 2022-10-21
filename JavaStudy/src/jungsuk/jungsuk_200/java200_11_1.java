563932f73b603dfd3f8062787163d9131585ef44
Shjung <wjdtngus9@gmail.com>
2022-10-21 23:57:58 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_11_1.java
@@ -0,0 +1,28 @@
+package jungsuk.jungsuk_200;
+import java.util.*;
+
+public class java200_11_1 {
+
+	public static void main(String[] args) {
+		ArrayList list1 = new ArrayList();
+		ArrayList list2 = new ArrayList();
+		ArrayList kyo = new ArrayList(); // 교집합
+		ArrayList cha = new ArrayList(); // 차집합
+		ArrayList hap = new ArrayList(); // 합집합
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+		list2.add(3);
+		list2.add(4);
+		list2.add(5);
+		list2.add(6);
+		
+		System.out.println("list1="+list1);
+		System.out.println("list2="+list2);
+		System.out.println("kyo="+kyo);
+		System.out.println("cha="+cha);
+		System.out.println("hap="+hap);
+	}
+
+}
