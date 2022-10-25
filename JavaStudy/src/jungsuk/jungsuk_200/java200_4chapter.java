fcd6b08e8bb3f1e6f745d39990527c14233f75c7
Shjung <wjdtngus9@gmail.com>
2022-10-25 23:59:11 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_4chapter.java
@@ -0,0 +1,14 @@
+package jungsuk.jungsuk_200;
+
+public class java200_4chapter {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			for(int j=0; j<=i; j++)
+			System.out.print("*");
+			System.out.println();
+		}
+
+	}
+
+}
