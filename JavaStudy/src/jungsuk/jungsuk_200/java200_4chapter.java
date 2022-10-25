d3dc418cbfbd7b1303288d338238490eb311111f
Shjung <wjdtngus9@gmail.com>
2022-10-26 00:11:04 +0900

    .

--- JavaStudy/src/jungsuk/jungsuk_200/java200_4chapter.java
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_4chapter.java
@@ -5,8 +5,18 @@ public class java200_4chapter {
 	public static void main(String[] args) {
 		for(int i=0; i<=10; i++) {
 			for(int j=0; j<=i; j++)
-			System.out.print("*");
+				System.out.print("*");
 			System.out.println();
+		}
+		
+		int i = 0;
+		
+		while(i<=10) {
+			for(int j = 0 ; j <= i ; j ++ ){
+			System.out.print("*");
+			}
+			System.out.println();
+			i++;
 		}
 
 	}
