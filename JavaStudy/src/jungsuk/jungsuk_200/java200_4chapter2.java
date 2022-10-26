67241bdc48551540a614aa249a3d8e96cc6db900
Shjung <wjdtngus9@gmail.com>
2022-10-26 23:58:23 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_4chapter2.java
@@ -0,0 +1,22 @@
+package jungsuk.jungsuk_200;
+
+public class java200_4chapter2 {
+
+	public static void main(String[] args) {
+		int num = 12345;
+		int sum = 0;
+		
+		System.out.println("sum="+sum);
+
+		// Fibonnaci 1, 1 . 수열의 시작의 첫 두 숫자를 로 한다
+		int num1 = 1;
+		int num2 = 1;
+		int num3 = 0; // 세번째 값
+		System.out.print(num1+","+num2);
+		for (int i = 0 ; i < 8 ; i++ ) {
+		
+		}
+
+	}
+
+}
