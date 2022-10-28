61f88857a425928f23d314a7ca46bf221e88dc9b
Shjung <wjdtngus9@gmail.com>
2022-10-28 23:55:47 +0900

    .

---
+++ JavaStudy/src/jungsuk/jungsuk_200/java200_4chapter3.java
@@ -0,0 +1,19 @@
+package jungsuk.jungsuk_200;
+
+public class java200_4chapter3 {
+
+	public static void main(String[] args) {
+		int number = 12321;
+		int tmp = number;
+		int result =0; // number 변수 를 거꾸로 변환해서 담을 변수
+		while(tmp !=0) {
+	
+		}
+		if(number == result)
+		System.out.println( number + " 는 회문수 입니다."); 
+		else
+		System.out.println( number + " 는 회문수가 아닙니다.");
+
+	}
+
+}
