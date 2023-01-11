36a4c00114cbf3148e9acd94bd99bd9f90e90040
Shjung <wjdtngus9@gmail.com>
2023-01-11 21:41:18 +0900

    .

---
+++ object-oriented-programming-2/src/com/in28minutes/oops/level2/FanRunner.java
@@ -0,0 +1,11 @@
+package com.in28minutes.oops.level2;
+
+public class FanRunner {
+
+	public static void main(String[] args) {
+		Fan fan = new Fan("Manufacturer 1", 0.34567, "GREEN");
+
+		System.out.println(fan);
+	}
+
+}
