5f46a0634cbeb6f0c7aadfff318e89dddb8ffa84
Shjung <wjdtngus9@gmail.com>
2023-01-30 21:59:05 +0900

    .

---
+++ JavaStudy/src/programmers/lessons12951.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class lessons12951 {
+
+	public static void main(String[] args) {
+		/*
+		 * JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다. 단, 첫 문자가 알파벳이 아닐 때에는
+		 * 이어지는 알파벳은 소문자로 쓰면 됩니다. "3people unFollowed me" "3people Unfollowed Me"
+		 * "for the last week" "For The Last Week"
+		 */
+
+		String s = "3people unFollowed me";
+		String[] str = s.split(" ");
+		for (int i = 0; i < str.length; i++) {
+
+			System.out.println(ch1);
+		}
+
+	}
+
+}
