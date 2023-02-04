81e072eb3b53dc704b98c497a22872e55f441e48
Shjung <wjdtngus9@gmail.com>
2023-02-04 23:39:37 +0900

    .

--- functional-programming/src/com/in28minutes/functionalprogramming/FPNumberRunner2.java
+++ functional-programming/src/com/in28minutes/functionalprogramming/FPNumberRunner2.java
@@ -1,6 +1,7 @@
 package com.in28minutes.functionalprogramming;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FPNumberRunner2 {
@@ -13,7 +14,14 @@ public class FPNumberRunner2 {
 		list.stream().map(x -> x.toLowerCase()).forEach(x -> System.out.println(x));
 		list.stream().forEach(x -> System.out.println(x.length()));
 
+		List<Integer> list2 = List.of(23, 12, 34, 53);
 
+		list2.stream().filter(n -> n % 2 == 1).collect(Collectors.toList());
+		System.out.println(list2.stream().filter(n -> n % 2 == 1).collect(Collectors.toList()));
+
+		System.out.println(list2.stream().filter(n -> n % 2 == 0).collect(Collectors.toList()));
+
+		System.out.println(IntStream.range(1, 11).map(x -> x * x).boxed().collect(Collectors.toList()));
 	}
 
 }
---
+++ functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -0,0 +1,66 @@
+package com.in28minutes.functionalprogramming;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+//Stream<T> filter(Predicate<? super T> predicate); 
+
+/*
+ * @FunctionalInterface public interface Predicate<T> { boolean test(T t);
+ */
+class EvenNumberPredicate implements Predicate<Integer> { // Predicate는 입력값의 참, 거짓을 판별 == 필터링.
+
+	@Override
+	public boolean test(Integer number) {
+		return number % 2 == 0;
+	}
+
+}
+
+
+// void forEach(Consumer<? super T> action);
+/*
+ * @FunctionalInterface public interface Consumer<T> {
+ * 
+ * void accept(T t);
+ */
+class EachConsumer implements Consumer<Integer> { // Consumer 는 입력값을 소비 , 반환값 없음.
+
+	@Override
+	public void accept(Integer t) {
+		System.out.println(t);
+	}
+
+}
+
+
+//<R> Stream<R> map(Function<? super T, ? extends R> mapper);
+//R apply(T t);
+class NumberSquareMapper implements Function<Integer, Integer> { // 변형해줌.
+									// type : 입력값 출력값
+	@Override
+	public Integer apply(Integer t) {
+
+		return t * t;
+	}
+	
+}
+
+public class LambdaBehindTheScreensRunner {
+
+	public static void main(String[] args) {
+		List.of(23, 43, 34, 45,36,48).stream()
+				.filter(n -> n % 2 == 0).map(n -> n * n).forEach(e -> System.out.println(e));
+
+		
+		List.of(23, 43, 34, 45, 36, 48).stream()
+				.filter(new EvenNumberPredicate()).map(new NumberSquareMapper()).forEach(new EachConsumer());
+
+		
+
+	}
+
+}
