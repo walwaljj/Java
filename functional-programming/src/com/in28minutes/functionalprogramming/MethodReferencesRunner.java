4778e11b90f4295d696480a76a4b3c969e890b37
Shjung <wjdtngus9@gmail.com>
2023-02-04 23:59:48 +0900

    .

---
+++ functional-programming/src/com/in28minutes/functionalprogramming/MethodReferencesRunner.java
@@ -0,0 +1,39 @@
+package com.in28minutes.functionalprogramming;
+
+import java.util.List;
+
+public class MethodReferencesRunner {
+
+	public static void print(Integer number) {
+		System.out.println(number);
+	}
+
+	public static void main(String[] args) {
+
+
+		List.of("Ant", "Bat", "Cat", "Dog", "Elephant").stream().map(s -> s.length())
+				.forEach(s -> System.out.println(s));
+		System.out.println("-----------");
+		List.of("Ant", "Bat", "Cat", "Dog", "Elephant").stream().map(s -> s.length())
+				.forEach(System.out::println);
+					//	클레스 부분  //	메서드
+		System.out.println("-----------");
+		List.of("Ant", "Bat", "Cat", "Dog", "Elephant").stream().map(String::length)
+				.forEach(MethodReferencesRunner::print);// forEach(s -> MethodReferencesRunner.print(s))
+		
+		System.out.println("-----------");
+		Integer max = List.of(23,45,67,34).stream()
+							.filter(n -> n % 2 == 0)
+							.max((n1,n2) -> Integer.compare(n1, n2))
+							.orElse(0);// Default 값을 0으로.
+		System.out.println(max);
+		
+		System.out.println("-----------");
+		Integer max2 = List.of(23,45,67,34).stream()
+							.filter(n -> n % 2 == 0)
+							.max((n1,n2) -> Integer.compare(n1, n2))
+							.orElse(0);// Default 값을 0으로.
+		System.out.println(max2);
+	}
+
+}
