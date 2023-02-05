1124ae9b97ee9e283b7fb11ac9d7798e4a0c38ec
Shjung <wjdtngus9@gmail.com>
2023-02-06 02:01:40 +0900

    .

---
+++ Multithreading/src/CallableRunner.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class CallableTask implements Callable<String>{
+	
+	private String name;
+
+	public CallableTask(String name) {
+		this.name = name;
+		
+	}
+	
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return "Hello " + name;
+	}
+	
+}
+
+
+public class CallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		Future<String> welcomeFuture = executorService.submit(new CallableTask("gg")); // execute 가 아닌 submit 은
+																						// callable을 실행할 수 있음.
+		System.out.println("\n new CallableTask(\"gg\") executed ");
+		String welcomeMessage = welcomeFuture.get();
+		System.out.println(welcomeMessage);
+
+		System.out.println("\n Main completed");
+		executorService.shutdown();
+	}
+
+}
