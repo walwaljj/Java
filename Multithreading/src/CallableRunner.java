65e1e2a290a14fbd508de925575878f47bf90ee4
Shjung <wjdtngus9@gmail.com>
2023-02-06 02:01:52 +0900

    .

--- Multithreading/src/CallableRunner.java
+++ Multithreading/src/CallableRunner.java
@@ -29,7 +29,7 @@ public class CallableRunner {
 		Future<String> welcomeFuture = executorService.submit(new CallableTask("gg")); // execute 가 아닌 submit 은
 																						// callable을 실행할 수 있음.
 		System.out.println("\n new CallableTask(\"gg\") executed ");
-		String welcomeMessage = welcomeFuture.get();
+		String welcomeMessage = welcomeFuture.get(); // 결과값 반환. (ㅇㅖ외처리)
 		System.out.println(welcomeMessage);
 
 		System.out.println("\n Main completed");
